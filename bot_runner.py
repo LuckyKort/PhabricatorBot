b3bd17e9bd1a6c08e76fa93cfe6871bb803a6610
Юрий Истомин <madkort@icloud.com>
2020-11-24 16:20:10 +0300

    WIP GET Project ID

--- bot_runner.py
+++ bot_runner.py
@@ -26,7 +26,7 @@ def start(message):
                      '\nОсновные команды:'
                      '\n/start - отобразить текущее сообщение'
                      '\n/help - инструкция по применению'
-                     '\n/status - статус мониторинга(true - активен | false - приостановлен)'
+                     '\n/status - статус мониторинга (true - активен | false - приостановлен)'
                      '\n/schedule - запустить задачу по поиску задач'
                      '\n/unschedule - приостановить поиск задач'
                      '\n/reset - остановить поиск задач и удалить настройки'
@@ -34,14 +34,14 @@ def start(message):
                      '\n/settings - отобразить все настройки одним сообщением'
                      '\n/server - отобразить текущий адрес сервера,'
                      '\n/phab_api - отобразить текущий токен'
-                     '\n/frequency - отобразить текущую частоту обращения к серверу(в минутах)'
+                     '\n/frequency - отобразить текущую частоту обращения к серверу (в минутах)'
                      '\n/board_name - отобразить имя борды, за которой нужно следить'
                      '\n/ignored_boards - отобразить список идентификаторов бордов, '
                      '\nобновления в которых стоит игнорировать'
                      '\nНастройка:'
                      '\n/server АдресСервера - задать адрес сервера'
                      '\n/phab_api API-токен - задать API-токен, выданный фабрикатором'
-                     '\n/frequency ЦЕЛОЕ - задать частоту обращения к серверу(в минутах)'
+                     '\n/frequency ЦЕЛОЕ - задать частоту обращения к серверу (в минутах)'
                      '\n/board_name ИмяБорды - задать имя борды, за которой нужно следить'
                      '\n/ignored_boards Ид1 Ид2 ... - задать список идентификаторов бордов, '
                      '\nобновления в которых стоит игнорировать'
@@ -66,23 +66,54 @@ def reset(message):
 
 @bot.message_handler(commands=['status'])
 def status(message):
-    bot.send_message(message.chat.id,
-                     "Статус мониторинга: %r" % config.active(message.chat.id))
+    bot.send_message(message.chat.id, "Статус мониторинга: %r" % config.active(message.chat.id))
+
+
+@bot.message_handler(commands=['project_id'])
+def get_project(message):
+    args = __extract_args(message.text)
+    if args is not None:
+        args = ' '.join(args)
+        url = '{0}/api/project.search'.format(config.server(message.chat.id))
+        data = {
+            "api.token": config.phab_api(message.chat.id),
+            "constraints[name]": args,
+        }
+        r = requests.post(url, params=data, verify=False)
+        result = r.json()
+        if len(result['result']['data']) > 0:
+            resultstr = 'Результат поиска:\n'
+            for i in range(len(result['result']['data'])):
+                phid = result['result']['data'][i]['phid']
+                depth = result['result']['data'][i]['fields']['depth']
+                pname = (result['result']['data'][i]['fields']['parent']['name']) if depth != 0 else None
+                name = result['result']['data'][i]['fields']['name']
+                active = result['result']['data'][i]['fields']['color']['key']
+                resultname = ((pname + " - ") if int(depth) > 1 else "") + name
+                resultstr += (resultname + ", " + phid + "\n") if active != "disabled" else None
+                #TODO
+            bot.send_message(message.chat.id, resultstr)
+        else:
+            bot.send_message(message.chat.id, "Проекты с таким именем не найдены")
+    else:
+        bot.send_message(message.chat.id, "Введите название проекта!")
 
 
 @bot.message_handler(commands=['settings'])
 def settings(message):
     bot.send_message(message.chat.id,
-                     ("Адрес сервера: %s\n" 
-                      "API токен в фабрикаторе: %s\n" 
-                      "Частота опроса сервера(минуты): %s\n" 
-                      "Имя борды: %s\n" 
-                      "Идентификаторы игнорируемых бордов: %s") % (
+                     ("* Адрес сервера: %s\n" 
+                      "* API токен в фабрикаторе: %s\n" 
+                      "* Частота опроса сервера (минуты): %s\n" 
+                      "* Имя борды: %s\n" 
+                      "* Идентификаторы игнорируемых бордов: \n%s\n" 
+                      "* Названия игнорируемых колонок: \n%s") % (
                       config.server(message.chat.id),
                       config.phab_api(message.chat.id),
                       config.frequency(message.chat.id),
                       config.board_name(message.chat.id),
-                      config.ignored_boards(message.chat.id)
+                      ','.join(config.ignored_boards(message.chat.id)),
+                      ','.join(config.ignored_columns(message.chat.id)),
                      ))
 
 
@@ -91,8 +122,7 @@ def server(message):
     args = __extract_args(message.text)
     if args:
         config.set_server(message.chat.id, args[0])
-    bot.send_message(message.chat.id,
-                     "Адрес сервера: %s" % config.server(message.chat.id))
+    bot.send_message(message.chat.id, "Адрес сервера: %s" % config.server(message.chat.id))
 
 
 @bot.message_handler(commands=['phab_api'])
@@ -100,8 +130,7 @@ def phab_api(message):
     args = __extract_args(message.text)
     if args:
         config.set_phab_api(message.chat.id, args[0])
-    bot.send_message(message.chat.id,
-                     "API токен в фабрикаторе: %s" % config.phab_api(message.chat.id))
+    bot.send_message(message.chat.id, "API токен в фабрикаторе: %s" % config.phab_api(message.chat.id))
 
 
 @bot.message_handler(commands=['frequency'])
@@ -109,8 +138,7 @@ def frequency(message):
     args = __extract_args(message.text)
     if args:
         config.set_frequency(message.chat.id, int(args[0]))
-    bot.send_message(message.chat.id,
-                     "Частота опроса сервера(минуты): %d" % config.frequency(message.chat.id))
+    bot.send_message(message.chat.id, "Частота опроса сервера (минуты): %d" % config.frequency(message.chat.id))
 
 
 @bot.message_handler(commands=['board_name'])
@@ -118,8 +146,7 @@ def board_name(message):
     args = __extract_args(message.text)
     if args:
         config.set_board_name(message.chat.id, args[0])
-    bot.send_message(message.chat.id,
-                     "Имя борды: %s" % config.board_name(message.chat.id))
+    bot.send_message(message.chat.id, "Имя борда: %s" % config.board_name(message.chat.id))
 
 
 @bot.message_handler(commands=['ignored_boards'])
@@ -127,14 +154,43 @@ def ignored_boards(message):
     args = __extract_args(message.text)
     if args:
         config.set_ignored_boards(message.chat.id, args)
-    bot.send_message(message.chat.id,
-                     "Идентификаторы игнорируемых бордов: %s" % config.ignored_boards(message.chat.id))
+    if len(config.ignored_boards(message.chat.id)) > 0:
+        ignored_boards_list = ','.join(config.ignored_boards(message.chat.id))
+    else:
+        ignored_boards_list = 'Список пуст'
+    bot.send_message(message.chat.id, "Идентификаторы игнорируемых бордов: \n%s" % ignored_boards_list)
+
+
+@bot.message_handler(commands=['reset_ignored_boards'])
+def ignored_boards(message):
+    config.unset_ignored_boards(message.chat.id)
+    bot.send_message(message.chat.id, "Идентификаторы игнорируемых бордов сброшены")
+
+
+@bot.message_handler(commands=['ignored_columns'])
+def ignored_columns(message):
+    args = __extract_args(message.text)
+    if args:
+        args = ' '.join(args).split(',')
+        config.set_ignored_columns(message.chat.id, args)
+    if len(config.ignored_columns(message.chat.id)) > 0:
+        ignored_columns_list = ','.join(config.ignored_columns(message.chat.id))
+    else:
+        ignored_columns_list = 'Список пуст'
+    bot.send_message(message.chat.id, "Названия игнорируемых колонок: \n%s" % ignored_columns_list)
+
+
+@bot.message_handler(commands=['reset_ignored_columns'])
+def ignored_boards(message):
+    config.unset_ignored_columns(message.chat.id)
+    bot.send_message(message.chat.id, "Имена игнорируемых бордов сброшены")
+
 
 @bot.message_handler(commands=['last_check'])
 def last_check(message):
     bot.send_message(message.chat.id,
                      "Время последней проверки на наличие новых тасков: %s\n"
-                     "Время последней проверки на наличие обновленных тасков: %s"% (
+                     "Время последней проверки на наличие обновленных тасков: %s" % (
                         config.last_new_check(message.chat.id), 
                         config.last_update_check(message.chat.id)))
 
